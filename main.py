c2c3bea623945c82a3fa8113c109d3e1462e31a5
inf <manskiy2k@gmail.com>
2024-07-31 20:03:59 +0300

    #11 удалил функцию load_keys и перенёс код в def main для упрощения. #17 удалил функцию requests_img и перенёс код в def main для упрощения. #23 удалил функцию save_img  и перенёс код в def main для упрощения. #36 удалил функцию create_full_path - не нужный код. #43 удалил функцию open_file и перенёс код в def main для упрощения. #48 переименовал функцию для информативности. Добавил переменную total_comics_pages для указания макс. числа страниц комикса. #49

--- main.py
+++ main.py
@@ -8,44 +8,7 @@ from pathlib import Path
 from telegram.error import NetworkError
 
 
-def load_keys(token):
-    load_dotenv()
-    key = os.environ[token]
-    return key
-
-
-def requests_img(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.json()
-
-
-def save_img(url, filename, image_format="jpeg", path="Files"):
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    if path == "Files":
-        full_path = os.path.join(script_dir, path, f"{filename}")
-    else:
-        full_path = os.path.join(path, f"{filename}")
-    os.makedirs(path, exist_ok=True)
-    response = requests.get(url)
-    response.raise_for_status()
-    with open(f"{full_path}.{image_format}", 'wb') as file:
-        file.write(response.content)
-
-
-def create_full_path(file_name, image_path):
-    os.makedirs(image_path, exist_ok=True)
-    for path in Path(image_path).rglob('{}.{}'.format(file_name, "*")):
-        if path.stem == str(file_name):
-            return path.resolve()
-
-
-def open_file(file):
-    with open(file, 'rb') as photo:
-        return photo.read()
-
-
-def trying_send_tg_message(load_key, path_photo, token, message):
+def try_send_tg_message(load_key, path_photo, token, message):
     bot = telegram.Bot(token=token)
     retries = 0
     for attempt in range(31):
@@ -61,15 +24,30 @@ def trying_send_tg_message(load_key, path_photo, token, message):
 
 
 def main():
-    tg_channel = load_keys("TG_CHANNEL")
-    tg_key = load_keys("TG_KEY")
-    page = random.randrange(2965)
+    load_dotenv()
+    tg_channel = os.environ["TG_CHANNEL"]
+    tg_key = os.environ["TG_KEY"]
+    total_comics_pages = 2965
+    path = "Files"
+    page = random.randrange(total_comics_pages)
     url = f'https://xkcd.com/{page}/info.0.json'
-    resp = requests_img(url)
-    save_img(resp['img'], page)
-    photo_path = create_full_path(file_name=page, image_path='Files')
-    trying_send_tg_message(tg_channel, open_file(photo_path), tg_key, resp['alt'])
-    Path.unlink(photo_path)
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        response = response.json()
+        response_img = requests.get(response['img'])
+        response_img.raise_for_status()
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        full_path = os.path.join(script_dir, path, f"{page}")
+        os.makedirs(path, exist_ok=True)
+        photo_path = f"{full_path}.jpeg"
+        with open(photo_path, 'wb') as file:
+            file.write(response_img.content)
+        with open(photo_path, 'rb') as photo:
+            photo = photo.read()
+            try_send_tg_message(tg_channel, photo, tg_key, response['alt'])
+    finally:
+        Path.unlink(photo_path)
 
 
 if __name__ == '__main__':
